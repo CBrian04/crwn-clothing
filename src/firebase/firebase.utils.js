0c397857f29ec92c45ee517578cd012998ba77d2
BrianCharles04 <charlesbrian04700@gmail.com>
2020-04-03 08:07:15 +0200

    implementation des outils firebase, incluant la possibilité de stocker notre utilisateur authentifié dans une base de donnée firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,7 +11,33 @@ const config = {
     messagingSenderId: "671209137645",
     appId: "1:671209137645:web:412cf5ef618cbf90f873e4",
     measurementId: "G-SBHY90KS4X"
-  }
+  };
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+      if(!userAuth) return;
+
+      const userRef= firestore.doc(`users/${userAuth.uid}`);
+
+      const snapshot = await userRef.get();
+
+      if(!snapshot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+      }
+
+      return userRef;
+  };
 
   firebase.initializeApp(config);
 
