7535becbd1222d35efddcce2e9834e23e4d31d54
BrianCharles04 <charlesbrian04700@gmail.com>
2020-04-11 23:08:48 +0200

    conversion de tous les styles.scss en styles.jsx (css in js), stockage des shop datas dans firebase store, qui sont désormais atteinte en allant chercher dans la base de données et qui sont stockées dans le shop reducer de notre shop page. On a aussi mis en place un HOC with spinner pour animer le temps d'attente

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
- 
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selector'    // add actions here
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
- 
-import './collections-overview.styles.scss';
- 
-const CollectionsOverview = ({collections}) => {
-  return (
-    <div className="collecitons-overview">
-        {collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-    </div>
-  )
-}
- 
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
+
+import { CollectionsOverviewContainer } from './collections-overview.styles';
+
+const CollectionsOverview = ({ collections }) => (
+  <CollectionsOverviewContainer>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </CollectionsOverviewContainer>
+);
+
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollectionsForPreview
+  collections: selectCollectionsForPreview
 });
- 
+
 export default connect(mapStateToProps)(CollectionsOverview);
