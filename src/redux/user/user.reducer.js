0c58ffc072d7f360d9f659da0d3f21575b7a709a
BrianCharles04 <charlesbrian04700@gmail.com>
2020-04-19 12:23:33 +0200

    creation des user et cart sagas pour sign in, sign up

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,11 +1,12 @@
 import UserActionTypes from "./user.types";
 
 const INITIAL_STATE = {
-    currentUser : null
+    currentUser: null
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case UserActionTypes.SIGN_UP_SUCCESS:
         case UserActionTypes.SIGN_IN_SUCCESS:
             return {
                 ...state,
@@ -19,6 +20,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
                 error: null
             };
         case UserActionTypes.SIGN_IN_FAILURE:
+        case UserActionTypes.SIGN_UP_FAILURE:
         case UserActionTypes.SIGN_OUT_FAILURE:
             return {
                 ...state,
